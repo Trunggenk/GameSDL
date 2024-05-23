f1c0731aaee9cbf0f6733fe68de276474cdcf745
Tống Quang Trung <114966956+Trunggenk@users.noreply.github.com>
2024-05-23 21:06:01 +0700

    w

--- Time.h
+++ Time.h
@@ -1,37 +1,79 @@
-#ifndef TIME_MANAGE_H_
-#define TIME_MANAGE_H_
+#include "SDL_Init.h"
 
 class Time {
 public:
     Time();
     ~Time();
-
-
-    void begin();
-    void run_game();
-
-
+    
+    void start();
+    void stop();
     void pause();
-    void end();
-    int get_time();
-
-    void unpause();
-
-
-    bool game_started();
-
-    bool game_paused();
-
-    int time_played();
+    void resume();
+    int getTime();
+    bool isGameRunning();
+    bool isGamePaused();
+    int getTimePlayed();
 
 private:
-    int startTime;
-    int pausedTime;
+    Uint32 startTime;
+    Uint32 gameStartTime;
+    Uint32 totalTime;
+    bool isRunning;
     bool isPaused;
-    bool isStarted;
-    int gameStartTime;
-    int totalTime;
-
 };
 
-#endif
+Time::Time() {
+    startTime = 0;
+    gameStartTime = 0;
+    totalTime = 0;
+    isRunning = false;
+    isPaused = false;
+}
+
+Time::~Time() {}
+
+void Time::start() {
+    isRunning = true;
+    isPaused = false;
+    startTime = SDL_GetTicks();
+    gameStartTime = startTime;
+}
+
+void Time::stop() {
+    isRunning = false;
+    isPaused = false;
+}
+
+void Time::pause() {
+    if (isRunning && !isPaused) {
+        isPaused = true;
+        totalTime += SDL_GetTicks() - startTime;
+    }
+}
+
+void Time::resume() {
+    if (isPaused) {
+        isPaused = false;
+        startTime = SDL_GetTicks();
+    }
+}
+
+int Time::getTime() {
+    if (isRunning) {
+        if (isPaused) return totalTime;
+        else return SDL_GetTicks() - startTime + totalTime;
+    }
+    return 0;
+}
+
+bool Time::isGameRunning() {
+    return isRunning;
+}
+
+bool Time::isGamePaused() {
+    return isPaused;
+}
+
+int Time::getTimePlayed() {
+    return SDL_GetTicks() - gameStartTime;
+}
